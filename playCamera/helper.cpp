6e5d3b9ded8e61cc29a7d9d71471c7d406506e3c
Rufus <rcywongaa@gmail.com>
2016-08-12 19:00:08 +0800

    Use absdiff in calculateDifference Put CHAIR_COLORS, TABLE_COLORS definition in cpp

--- playCamera/helper.cpp
+++ playCamera/helper.cpp
@@ -1,5 +1,64 @@
 #include "helper.h"
 
+//6:00pm
+/*
+cv::Scalar TABLE_COLOR1(0, 0, 0);
+cv::Scalar TABLE_COLOR2(40, 40, 60);
+cv::Scalar CHAIR_COLOR1(0, 40, 0);
+cv::Scalar CHAIR_COLOR2(40, 100, 75);
+*/
+//6:30pm
+/*
+cv::Scalar TABLE_COLOR1(0, 0, 0);
+cv::Scalar TABLE_COLOR2(40, 50, 60);
+cv::Scalar CHAIR_COLOR1(0, 50, 0);
+cv::Scalar CHAIR_COLOR2(30, 120, 75);
+*/
+//7:00pm
+/*
+cv::Scalar TABLE_COLOR1(0, 0, 0);
+cv::Scalar TABLE_COLOR2(255, 50, 60);
+cv::Scalar CHAIR_COLOR1(0, 30, 0);
+cv::Scalar CHAIR_COLOR2(30, 120, 75);
+*/												 
+cv::Scalar TABLE_COLOR1_1800(0, 0, 0);
+cv::Scalar TABLE_COLOR2_1800(40, 50, 60);
+cv::Scalar CHAIR_COLOR1_1800(0, 40, 0);
+cv::Scalar CHAIR_COLOR2_1800(40, 120, 75);
+
+//Living
+cv::Scalar LIVING_TABLE_COLOR1(0, 50, 0);
+cv::Scalar LIVING_TABLE_COLOR2(15, 175, 75);
+cv::Scalar LIVING_CHAIR_COLOR1(0, 150, 50);
+cv::Scalar LIVING_CHAIR_COLOR2(15, 200, 175);
+
+//Dining
+cv::Scalar DINING_TABLE_COLOR1(0, 50, 0);
+cv::Scalar DINING_TABLE_COLOR2(15, 125, 75);
+cv::Scalar DINING_CHAIR_COLOR1(0, 125, 50);
+cv::Scalar DINING_CHAIR_COLOR2(15, 175, 150);
+
+//Study
+cv::Scalar STUDY_TABLE_COLOR1(0, 0, 0);
+cv::Scalar STUDY_TABLE_COLOR2(255, 255, 50);
+cv::Scalar STUDY_CHAIR_COLOR1(0, 0, 25);
+cv::Scalar STUDY_CHAIR_COLOR2(30, 75, 100);
+
+vector<tuple<cv::Scalar, cv::Scalar>> CHAIR_COLORS(NUM_ROOMS);
+vector<tuple<cv::Scalar, cv::Scalar>> TABLE_COLORS(NUM_ROOMS);
+CHAIR_COLORS[LIVING] = tuple<cv::Scalar, cv::Scalar>(LIVING_CHAIR_COLOR1, LIVING_CHAIR_COLOR2);
+TABLE_COLORS[LIVING] = tuple<cv::Scalar, cv::Scalar>(LIVING_TABLE_COLOR1, LIVING_TABLE_COLOR2);
+CHAIR_COLORS[UNDEFINED] = tuple<cv::Scalar, cv::Scalar>(LIVING_CHAIR_COLOR1, LIVING_CHAIR_COLOR2);
+TABLE_COLORS[UNDEFINED] = tuple<cv::Scalar, cv::Scalar>(LIVING_TABLE_COLOR1, LIVING_TABLE_COLOR2);
+CHAIR_COLORS[DINING] = tuple<cv::Scalar, cv::Scalar>(DINING_CHAIR_COLOR1, DINING_CHAIR_COLOR2);
+TABLE_COLORS[DINING] = tuple<cv::Scalar, cv::Scalar>(DINING_TABLE_COLOR1, DINING_TABLE_COLOR2);
+CHAIR_COLORS[STUDY] = tuple<cv::Scalar, cv::Scalar>(STUDY_CHAIR_COLOR1, STUDY_CHAIR_COLOR2);
+TABLE_COLORS[STUDY] = tuple<cv::Scalar, cv::Scalar>(STUDY_TABLE_COLOR1, STUDY_TABLE_COLOR2);
+CHAIR_COLORS[ON] = tuple<cv::Scalar, cv::Scalar>(CHAIR_COLOR1_1800, CHAIR_COLOR2_1800);
+TABLE_COLORS[ON] = tuple<cv::Scalar, cv::Scalar>(TABLE_COLOR1_1800, TABLE_COLOR2_1800);
+CHAIR_COLORS[OFF] = tuple<cv::Scalar, cv::Scalar>(CHAIR_COLOR1_1800, CHAIR_COLOR2_1800);
+TABLE_COLORS[OFF] = tuple<cv::Scalar, cv::Scalar>(TABLE_COLOR1_1800, TABLE_COLOR2_1800);
+
 vector<cv::Mat> generateTemplates(cv::Mat input)
 {
 	float PAD_SCALE = 1.5;
@@ -282,7 +341,12 @@ Room getSimilarRoom(cv::Mat &img, vector<RoomPhotos> &vecRP)
 //TODO: Check values
 double calculateDifference(cv::Mat &img1, cv::Mat &img2)
 {
-	cv::Mat xor;
-	cv::bitwise_xor(img1, img2, xor);
-	return cv::mean(xor)[0];
+	cv::Mat diff;
+    cv::absdiff(img1, img2, diff);
+	//cv::bitwise_xor(img1, img2, diff);
+    cv::Mat diff_rgb[3];
+    cv::split(diff, diff_rgb);
+    cv::Mat total_diff = cv::max(diff_rgb[0], diff_rgb[1]);
+    total_diff = cv::max(total_diff, diff_rgb[2]);
+	return cv::mean(total_diff) / 255;
 }
